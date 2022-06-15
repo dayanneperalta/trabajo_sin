35c7d5fdcd7228199e79883e6c24656e1e710f27
Carlos Guarda <carlos.guarda@qbitla.com>
2022-06-15 01:36:16 -0500

    add htaccess file

--- php/connection.php
+++ php/connection.php
@@ -1,2 +1,8 @@
 <?php
-$conn = mysqli_connect("localhost", "trabajo_sin", "trabajo_sin", "sin_grupo_10");
+
+$mysql_user = getenv("PHP_MYSQL_USER");
+$mysql_password = getenv("PHP_MYSQL_PASSWORD");
+$mysql_host = getenv("PHP_MYSQL_HOST");
+$mysql_db = getenv("PHP_MYSQL_DB");
+
+$conn = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_db);
